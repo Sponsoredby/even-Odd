1fabb6665deeab2398bb5a6d9fc0d92c3a95dc85
Sponsoredby <74147554+Sponsoredby@users.noreply.github.com>
2020-11-13 12:24:12 +0000

    Add files via upload

---
+++ odd and even.py
@@ -0,0 +1,10 @@
+print ("Welcome to even and odd checker v 0.1a")
+x = input ("please enter a number: ")
+x = int (x)
+if x%2 == 0:
+    print ("x is even")
+else:
+    print ("x is odd")
+#a simple even and odd number checker taken from py4e
+# i added user input and converted string to interger
+#deliberate bugs number can be non numerical and program doesnt loop
