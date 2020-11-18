79b0b0e0bebcfc306470cf010500df8ef6e6ee2e
LevyJ <74147554+Sponsoredby@users.noreply.github.com>
2020-11-18 15:36:10 +0000

    Update odd and even.py

--- odd and even.py
+++ odd and even.py
@@ -2,10 +2,20 @@ print ("Welcome to even and odd checker v 0.1a")
 x = input ("please enter a number: ")
 x = int (x)
 
+#create a function here
+def num():
+    return num
 if x%2 == 0:
-    print (x, "is an even number")
+    print ('\n',x, "is an even number")
 else:
-    print (x, "is an odd number")
+    print ('\n',x, "is an odd number")
+
+if x == int(x):
+    print("\nEnter another number to try again or enter q to quit")
+
+num
+
+#exit - maybe once time has been learned make it so if no input is entered after set period then the program auto exits
 #a simple even and odd number checker taken from py4e
 # i added user input and converted string to interger
 #deliberate bugs number can be non numerical and program doesnt loop
